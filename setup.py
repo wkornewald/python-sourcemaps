a8228016b24bf2501130988cc70c84c824fb08e3
Waldemar Kornewald <wkornewald@gmail.com>
2015-02-02 10:28:48 +0100

    upload to pypi

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = 'Python library for parsing and generating source maps'
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.rst').read()
+except:
+    pass
+
+setup(name='python-sourcemaps',
+      version='1.0',
+      packages=find_packages(exclude=('tests', 'tests.*')),
+      author='Waldemar Kornewald',
+      author_email='wkornewald@gmail.com',
+      url='https://bitbucket.org/allbuttonspressed/python-sourcemaps',
+      license='BSD',
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      platforms=['any'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+          'License :: OSI Approved :: BSD License',
+      ],
+)
